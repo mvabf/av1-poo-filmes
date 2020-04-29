43c590ae5eb1d42ba09d15d5d6a64312ac951472
Marcus Baltazar <mvabf_dev@outlook.com>
2020-04-29 17:08:32 -0300

    Funcionalidade Procurar filme implementada

--- src/App/Programa.java
+++ src/App/Programa.java
@@ -12,6 +12,8 @@ public class Programa {
 		
 		Scanner in  = new Scanner(System.in);
 		
+		char escolha = 's';
+		
 		// Instancia da lista de filmes que serão Adcionados
 		List<Filme> filmes = new ArrayList<>();
 		
@@ -21,39 +23,52 @@ public class Programa {
 		System.out.println("L I S T A DE F I L M E S");
 		System.out.println("--------------------------------");
 		
-		System.out.println("Digite: ");
-		System.out.println("1 - Cadastrar Filme");
-		System.out.println("2 - Pesquisar Filme");
-		System.out.println("3 - Editar Filme");
-		System.out.println("4 - Deletar Filme");
-		
-		System.out.print("Insira sua opção: ");
-		int op = in.nextInt();
-		
-		// switch de acordo com as opções selecionadas
-		switch(op) {
-		// cadastro de filme
-		case 1:
-			System.out.println("Insira Título do Filme: ");
-			in.nextLine();
-			String titulo = in.nextLine();
-			System.out.println("Insira ano de lançamento: ");
-			int anoLancamento = in.nextInt();
-			in.nextLine();
-			System.out.println("Insira Gênero do filme: ");
-			String genero = in.nextLine();
-			System.out.println("Insira a descrição do filme: ");
-			String descricao = in.nextLine();
+		while(escolha == 's') {
+			System.out.println("Digite: ");
+			System.out.println("1 - Cadastrar Filme");
+			System.out.println("2 - Pesquisar Filme");
+			System.out.println("3 - Editar Filme");
+			System.out.println("4 - Deletar Filme");
 			
-			filmes.add(new Filme(titulo, anoLancamento, genero, descricao));
-		}
-		
-		// saída para testes
-		System.out.println("Lista de Filmes: ");
-		for(Filme f : filmes) {
-			System.out.println(f);
-		}
-		
+			System.out.print("Insira sua opção: ");
+			int op = in.nextInt();
+			
+			// switch de acordo com as opções selecionadas
+			switch(op) {
+			// cadastro de filme
+			case 1:
+				System.out.println("Insira Título do Filme: ");
+				in.nextLine();
+				String titulo = in.nextLine();
+				System.out.println("Insira ano de lançamento: ");
+				int anoLancamento = in.nextInt();
+				in.nextLine();
+				System.out.println("Insira Gênero do filme: ");
+				String genero = in.nextLine();
+				System.out.println("Insira a descrição do filme: ");
+				String descricao = in.nextLine();
+				
+				filmes.add(new Filme(titulo, anoLancamento, genero, descricao));
+				
+				break;
+				
+			case 2:
+				System.out.println("Insira o nome do filme qual deseja buscar: ");
+				String nome = in.next();
+				Filme f = filmes.stream().filter(x -> nome.equals(x.getTitulo())).findAny().orElse(null);
+				
+				if(f == null) {
+					System.out.println("Filme Não existe!");
+				} else {
+					System.out.println(f);
+				}
+				
+				break;
+			}
+			
+			System.out.println("Deseja Continuar? (s/n)");
+			escolha = in.next().charAt(0);
+	}
 		in.close();
 
 	}
--- src/entidades/Filme.java
+++ src/entidades/Filme.java
@@ -60,6 +60,31 @@ public class Filme {
 				+ "Descrição: "
 				+ descricao;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((titulo == null) ? 0 : titulo.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Filme other = (Filme) obj;
+		if (titulo == null) {
+			if (other.titulo != null)
+				return false;
+		} else if (!titulo.equals(other.titulo))
+			return false;
+		return true;
+	}
 	
 
 	
